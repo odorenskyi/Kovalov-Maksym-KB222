3a84157a3db0227895f7f0cdc2c604c913b9a88a
kov1k <k.maks.v@ukr.net>
2023-04-15 22:34:08 +0300

    Видалив не правильно працюючу бібліотеку

--- lab09/prj/ModulesKovalov/kovalov_lib.h
+++
@@ -1,6 +0,0 @@
-#ifndef KOVALOV_LIB_H_INCLUDED
-#define KOVALOV_LIB_H_INCLUDED
-
-float s_calculation(float x, float y, float z);
-float kwh_calculation(float kwh);
-#endif // KOVALOV_LIB_H_INCLUDED
