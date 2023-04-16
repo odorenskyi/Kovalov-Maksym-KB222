8a0999a45188d102b27c5fc23e7b1f70dde6c096
kov1k <k.maks.v@ukr.net>
2023-04-16 16:36:13 +0300

    9.1 Розробив тест драйвер

--- lab09/prj/TestDriver/main.cpp
+++ lab09/prj/TestDriver/main.cpp
@@ -1,15 +1,47 @@
 #include <iostream>
+#include <cmath>
 #include "../moduleskovalov/kovalov_lib.h"
+#define ARR_S 7
 using namespace std;
 
 int main()
 {
     setlocale(LC_ALL, "ukr");
-    float kwh, tempC, tempSum = 0;
+    float tempC, tempSum = 0;
     // 9.1
-    // Обрахунок кВт_год
-  /*  cin >> kwh;
-    cout << kwh_calculation(kwh) << endl; */
+    const float kwh[ARR_S] = {10, 151, -2, -14.45, 800, 806, 1000};
+    const float expected_res[ARR_S] = {13.0843, 365.337, 0, 0, 1935.56, 4304.42, 5340.47};
+    float eps = 0.002;
+    for(short int i = 0; i < ARR_S; i++){
+        float currResult = round(kwh_calculation(kwh[i])*10000)/10000;
+        cout << "Test #" << i + 1 << ": " << kwh[i] << " \n Очікуваний результат: " << expected_res[i]
+        << ",\n Дійсний результат: " << currResult << endl;
+
+        // допустима погрішність при зрівнюванні результатів
+        if(abs(currResult - expected_res[i]) < eps){
+            cout << "Test id #" << i+1 << " - Passed\n";
+        } else {
+            cout << "Test id #" << i+1 << " - Failed\n";
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     // 9.2
     // Обрахунок середньої температури за рік В цельсіях
@@ -30,7 +62,7 @@ int main()
     }
 */
     // 9.3
-    int N;
+  /*  int N;
     cin >> N;
     if(cin.fail()){
         std::cout << ("Введений неправильний тип даних. Введіть число.");
@@ -43,5 +75,5 @@ int main()
         cout << "Кількість двійкових нулів у N:"<< binary_calculation(N) << endl;
     }
 
-
+*/
 }
