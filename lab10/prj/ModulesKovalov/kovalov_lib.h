859ef5048d277ebad0b9a2367bba66bd43a07bbf
kov1k <k.maks.v@ukr.net>
2023-05-01 20:41:37 +0300

    Переніс статичну бібліотеку з 8 лабораторної

---
+++ lab10/prj/ModulesKovalov/kovalov_lib.h
@@ -0,0 +1,7 @@
+#ifndef KOVALOV_LIB_H_INCLUDED
+#define KOVALOV_LIB_H_INCLUDED
+
+float s_calculation(float, float, float);
+
+
+#endif // KOVALOV_LIB_H_INCLUDED
---
+++ lab10/prj/ModulesKovalov/main.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <iostream>
+// 8 Lab ----------
+const double pi = 3.14159;
+const double e = 2.71828;
+float s_calculation(float x, float y, float z){
+    double s=0;
+
+    if(x < 0){
+        std::cerr << "Помилка, знаменник x менше нуля" << std::endl;
+        return 1;
+    }
+    if (z == 0 || y == 0) {
+        std::cerr << "Помилка, sin не може дорівнювати 0" << std::endl;
+        return 1;
+    }
+
+    double temp = pow(e, abs(x)) + sqrt(abs(y - z) / sin(z * y));
+    if (temp < 0 || std::isnan(temp)) {
+        std::cerr << "Помилка, квадратний корінь від від'ємного числа" << std::endl;
+        return 1;
+    }
+
+        s = 0.5*((pow(y,2)+ 2*z)/sqrt(7*pi + x)) - sqrt(pow(e,abs(x))+ (sqrt(abs(y-z)))/sin(z*y));
+        return s;
+
+}
+// Lab 10 ----------
+
+
+
