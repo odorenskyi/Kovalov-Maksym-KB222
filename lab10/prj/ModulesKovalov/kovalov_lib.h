d34c5d587159a1ceb8299852f555ca36766fd99a
kov1k <k.maks.v@ukr.net>
2023-05-06 22:25:23 +0300

    Написав 10.1_2, треба вирішити проблему з кодуванням файла task10_1.txt

--- lab10/prj/ModulesKovalov/kovalov_lib.h
+++ lab10/prj/ModulesKovalov/kovalov_lib.h
@@ -1,7 +1,23 @@
+#include <string>
 #ifndef KOVALOV_LIB_H_INCLUDED
 #define KOVALOV_LIB_H_INCLUDED
 
+using namespace std;
+
+
 float s_calculation(float, float, float);
 
+struct PersonInfo {
+    string name;
+    string surname;
+    string organization;
+    string city;
+    string country;
+    int year;
+};
+void write_info(const char* filename, const PersonInfo& person);
+
+int consonants_num(const char* filename);
+void write_consosnants_num(const char* filename, int c_count);
 
 #endif // KOVALOV_LIB_H_INCLUDED
--- lab10/prj/ModulesKovalov/main.cpp
+++ lab10/prj/ModulesKovalov/main.cpp
@@ -1,5 +1,9 @@
 #include <cmath>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+#include <cstring>
 // 8 Lab ----------
 const double pi = 3.14159;
 const double e = 2.71828;
@@ -27,5 +31,61 @@ float s_calculation(float x, float y, float z){
 }
 // Lab 10 ----------
 
+// Структура для запису даних
+struct PersonInfo {
+    std::string name;
+    std::string surname;
+    std::string organization;
+    std::string city;
+    std::string country;
+    int year;
+};
 
+void write_info(const char* filename, const PersonInfo& person){
+    std::ofstream op_file(filename);
+    if(op_file){
+        op_file << "Авторська інформація:" << std::endl;
+        op_file << "Ім'я: " << person.name << std::endl;
+        op_file << "Прізвище: " << person.surname << std::endl;
+        op_file << "Організація: " << person.organization << std::endl;
+        op_file << "Місто: " << person.city << std::endl;
+        op_file << "Країна: " << person.country << std::endl;
+        op_file << "Рік: " << person.year << std::endl;
+    }else{
+        std::cerr << "Не вдалося відкрити цей файл: " << filename << std::endl;
+    }
+}
 
+// 2
+int consonants_num(const char* filename){
+    // Кількість приголосних з word.txt
+    std::ifstream input_f(filename); // Відкриваємо файл для читання word.txt
+    int c_count = 0;
+    if(input_f){
+        std::string ukr_word;
+        std::getline(input_f, ukr_word);
+
+        for (char c : ukr_word) {
+            if (!std::strchr("бвгґджзйклмнпрстфхцчшщь", c)) {
+                c_count++;
+        }
+    }
+    }else{
+      std::cerr << "Не вдалося відкрити цей файл: " << filename << std::endl;
+    }
+    input_f.close();
+    return c_count;
+}
+
+void write_consosnants_num(const char* filename, int c_count){
+    // Відкриваємо файл для запису task10_1.txt
+    std::ofstream output_f(filename, std::ios::out | std::ios::binary | std::ios::app);
+
+    // Дозапис кількості приголосних у task10_1.txt
+        if(output_f){
+            output_f << "Кількість приголосних: "<< c_count << std::endl;
+        }else{
+            std::cerr << "Не вдалося відкрити цей файл: " << filename << std::endl;
+        }
+    output_f.close();
+}
