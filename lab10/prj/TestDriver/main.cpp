66c5413546de55eaa960efd04b1c9353808807af
kov1k <k.maks.v@ukr.net>
2023-05-06 22:24:54 +0300

    Написав 10.1_2, треба вирішити проблему з кодуванням файла task10_1.txt

--- lab10/prj/TestDriver/main.cpp
+++ lab10/prj/TestDriver/main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <fstream>
 #include "../moduleskovalov/kovalov_lib.h"
+
 using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL, "ukr");
+    int cons_num;
+
+    PersonInfo person = {
+    "Максим",
+    "Ковальов",
+    "ЦНТУ",
+    "Кропивницький",
+    "Україна",
+    2023
+    };
+
+    write_info("task10_1.txt", person);
+
+    cons_num = consonants_num("word.txt");
+    cout << cons_num;
+    if(cons_num > 0){
+        write_consosnants_num("task10_1.txt", cons_num);
+    }else{
+        write_consosnants_num("task10_1.txt", 0);
+    }
 
 }
